7fbb347851171af086f08ab57f4607d3e36ef045
James Hume <james.hume@gmail.com>
2017-05-23 21:37:52 +0100

    Expanding on the Andriod input system notes

--- src/generate_site.sh
+++ src/generate_site.sh
@@ -1,2 +1,19 @@
 python generate_site.py
-#firefox ../__deployed/index.html
+
+
+# From: https://unix.stackexchange.com/questions/9605/how-can-i-detect-if-the-shell-is-controlled-from-ssh
+MY_SESSION_TYPE=
+if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+  MY_SESSION_TYPE=remote
+# many other tests omitted
+else
+  case $(ps -o comm= -p $PPID) in
+    sshd|*/sshd) MY_SESSION_TYPE=remote;;
+  esac
+fi
+
+echo "Session type is \"$MY_SESSION_TYPE\""
+if [ "$MY_SESSION_TYPE" != "remote" ]; then
+   echo "Launching browser..."
+   firefox ../__deployed/index.html
+fi
