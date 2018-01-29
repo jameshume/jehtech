aa74ce5f33a7726d912551ce661f0bb83cb5e9f7
james <james.hume@gmail.com>
2018-01-29 11:39:40 +0000

    Lots of misc changes

--- src/deploy_site.bat
+++ src/deploy_site.bat
@@ -1,6 +1,10 @@
 @echo off
 echo DEPOLYING SITE....
 echo ------------------
-set /p pass="Enter password: "
-python deploy_site.py %pass%
+REM https://stackoverflow.com/questions/664957/can-i-mask-an-input-text-in-a-bat-file
+powershell -Command $pword = read-host "Enter password" -AsSecureString ; ^
+    $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword) ; ^
+        [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR) > .tmp.txt
+set /p password=<.tmp.txt & del .tmp.txt
+python deploy_site.py %password%
 pause
