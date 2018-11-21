657ff31d7015335165ba32bb3dad6870d03cd49c
james.hume@gmail.com <james.hume@gmail.com>
2018-11-21 21:07:50 +0000

    more linalg nores and fixed rule of sarrus typo

--- src/test.py
+++ src/test.py
@@ -1,32 +1,29 @@
+import matplotlib.pyplot as pl
+import numpy as np
 
+pi2 = 2 * np.pi
+sigFreqHz = 2
+sampFreqHz = 2 * sigFreqHz
 
-# You're not actually adding a new axes.
-
-# Matplotlib is detecting that there's already a plot in that position and returning it instead of a new axes 
-# object.
-
-# (Check it for yourself. ax and newax will be the same object.)
-
-# There's probably not a reason why you'd want to, but here's how you'd do it.
-
-# (Also, don't call newax = plt.axes() and then call fig.add_subplot(newax) You're doing the same thing twice.)
-
-# Edit: With newer (>=1.2, I think?) versions of matplotlib, you can accomplish the same thing as the 
-# example below by using the label kwarg to fig.add_subplot. 
-# E.g. newax = fig.add_subplot(111, label='some unique string')
-
-import matplotlib.pyplot as plt
-
-fig = plt.figure()
+fig = pl.figure()
 ax = fig.add_subplot(1,1,1)
 ax.grid(True)
-# If you just call `plt.axes()` or equivalently `fig.add_subplot()` matplotlib  
-# will just return `ax` again. It _won't_ create a new axis unless we
-# call fig.add_axes() or reset fig._seen
-newax = fig.add_axes(ax.get_position(), frameon=False)
-newax.grid(True)
-ax.plot(range(10), 'r-')
-newax.plot(range(50), 'g-')
-newax.axis('equal')
 
-plt.show()
+x = np.linspace(0, pi2, 1000)
+y = np.sin(sigFreqHz * x)
+ax.plot(x,y)
+
+xticks = np.linspace(0., 10., 9) / 10.
+ax.set_xticks( xticks * pi2)
+pl.xticks(rotation=45)
+ax.set_xticklabels([str(x) for x in xticks])
+
+x2 = np.linspace(0, 2*np.pi, 8)
+y2 = np.sin(sigFreqHz * x2)
+for x,y in zip(x2,y2):
+    ax.plot(x,y, marker='o', color='k')
+
+print(0.5 * (x2[1] - x2[0]))
+
+
+pl.show()
