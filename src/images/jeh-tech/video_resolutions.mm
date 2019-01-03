0e444974c4013c9b3980435850108f64eff0347f
James Hume <jh@ITDEV.local>
2019-01-03 07:33:17 +0000

    Video notes update

---
+++ src/images/jeh-tech/video_resolutions.mm
@@ -0,0 +1,38 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1545653403072" ID="ID_1658170709" MODIFIED="1545653976919" TEXT="Video">
+<font NAME="Calibri" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1545653421107" ID="ID_367498337" MODIFIED="1545653976909" POSITION="right" TEXT="Resolution">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="Calibri" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1545653436713" ID="ID_1531743434" MODIFIED="1545653976910" TEXT="SD">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Calibri" SIZE="16"/>
+<node COLOR="#990000" CREATED="1545653439861" ID="ID_1112542459" MODIFIED="1545653976910" TEXT="EU - 720 x 576i">
+<font NAME="Calibri" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1545653454317" ID="ID_589242538" MODIFIED="1545653976914" TEXT="USA - 720 x 480i">
+<font NAME="Calibri" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1545653466643" ID="ID_726244033" MODIFIED="1545653976915" TEXT="HD">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Calibri" SIZE="16"/>
+<node COLOR="#990000" CREATED="1545653471028" ID="ID_952778475" MODIFIED="1545653976916" TEXT="1280 x 720p">
+<font NAME="Calibri" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1545653479044" ID="ID_578248452" MODIFIED="1545653976917" TEXT="1920 x 1080i/p">
+<font NAME="Calibri" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1545653488939" ID="ID_1965014882" MODIFIED="1545653976918" STYLE="fork" TEXT="UHD (4K)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Calibri" SIZE="16"/>
+<node COLOR="#990000" CREATED="1545653498020" ID="ID_701689367" MODIFIED="1545653976918" TEXT="3840 x 2160p">
+<font NAME="Calibri" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
