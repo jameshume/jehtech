4386fe604dc6906690457eb495d44d5da7b3b46d
James Hume <james.hume@gmail.com>
2024-01-06 21:10:46 +0000

    Cap pulse animation

--- src/images/jeh-tech/ltspice/electronics_pulse_into_cap.py
+++ src/images/jeh-tech/ltspice/electronics_pulse_into_cap.py
@@ -1,7 +1,7 @@
 
 #pip install ltspice spicelib
+#conda install -c conda-forge ffmpeg
 import ltspice
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import sys  
@@ -9,6 +9,23 @@ import tempfile
 import shutil
 import subprocess
 from spicelib import AscEditor 
+import matplotlib.pyplot as pl
+import matplotlib.animation as animation
+
+
+fig, (ax,ax_i) = pl.subplots(nrows=2)
+line_V_source  = None
+line_V_cap     = None
+line_current   = None
+
+ax.set_ylabel("Voltage (Volts)")
+ax.grid()
+
+ax_i.set_xlabel("Time (Seconds)")
+ax_i.set_ylabel("Current (Amps)")
+ax_i.grid()
+
+NUM_FRAMES=250
 
 with tempfile.TemporaryDirectory() as tmpdirname:
     shutil.copy("../electronics_pulse_into_cap.asc", tmpdirname)
@@ -16,28 +33,53 @@ with tempfile.TemporaryDirectory() as tmpdirname:
 
     asc = AscEditor(f"{tmpdirname}/electronics_pulse_into_cap.asc") 
     print(asc.get_component_info("C1"))
-    asc.set_component_value("C1", "0.5mF")
-    asc.save_netlist(f"{tmpdirname}/electronics_pulse_into_cap_mod.asc")
 
-    x = subprocess.run(
-        ["C:\\Users\\JamesHume\\AppData\\Local\\Programs\\ADI\\LTspice\\ltspice.exe", 
-        "-Run", 
-        "-b", 
-        f"{tmpdirname}\\electronics_pulse_into_cap_mod.asc"], shell=True)
-    print(x)
-    print(os.listdir(tmpdirname))
+    def animate(i):
+        global line_V_source
+        global line_V_cap
+        global line_current
+        global fig
+        global ax
+        global ax_i
 
-    l = ltspice.Ltspice(f"{tmpdirname}/electronics_pulse_into_cap_mod.raw") 
-    # Make sure that the .raw file is located in the correct path
-    l.parse() 
+        print(i)
+        if i < NUM_FRAMES/2:
+            i = i * 50
+        else:
+            i = (NUM_FRAMES - i) * 50
+      
+        ax.set_title(f"C1 = {i/1000:.3f}mF")
+        asc.set_component_value("C1", f"{i}uF")
+        asc.save_netlist(f"{tmpdirname}/electronics_pulse_into_cap_mod.asc")
 
-    time = l.get_time()
-    V_source = l.get_data('V(source)')
-    V_cap = l.get_data('V(cap)')
+        x = subprocess.run(
+            ["C:\\Users\\JamesHume\\AppData\\Local\\Programs\\ADI\\LTspice\\ltspice.exe", 
+                "-Run", 
+                "-b", 
+                f"{tmpdirname}\\electronics_pulse_into_cap_mod.asc"
+            ], shell=True)
 
-    plt.plot(time, V_source)
-    plt.plot(time, V_cap)
-    plt.show()
+        l = ltspice.Ltspice(f"{tmpdirname}/electronics_pulse_into_cap_mod.raw") 
+        l.parse() 
 
+        time = l.get_time()
+        V_source = l.get_data('V(source)')
+        I_source = l.get_data('I(R1)')
+        V_cap = l.get_data('V(cap)')
 
+        if line_V_source is None:
+            line_V_source, = ax.plot(time, V_source)
+            line_V_cap, = ax.plot(time, V_cap)
+            line_current, = ax_i.plot(time, I_source)
+        else:
+            line_V_source.set_xdata([time])
+            line_V_source.set_ydata([V_source])
+            line_V_cap.set_xdata([time])
+            line_V_cap.set_ydata([V_cap])
+            line_current.set_xdata([time])
+            line_current.set_ydata([I_source])
 
+    Writer = animation.writers['ffmpeg']
+    writer = Writer(fps=15, metadata=dict(artist='James Hume @ www.jehtech.com'), bitrate=1800)
+    ani = animation.FuncAnimation(fig, animate, frames=NUM_FRAMES, blit=False, repeat=False)
+    ani.save('electronics_pulse_into_cap_vary_capacitance.mp4', writer=writer)        
